6a2d69ef0912e40fa39f8f2b5a658cbddee0051e
branhoff <branhoff2@gmail.com>
2021-01-01 21:29:07 -0800

    first lesson folder

---
+++ 01-lessons/01/calc.py
@@ -0,0 +1,14 @@
+def mult(num_1, num_2):
+    """
+    Returns the product of two numbers
+    """
+    return num_1 * num_2
+  
+def add(num_1, num_2):
+    """
+    Returns the sum of two numbers
+    """
+    return num_1 + num_2
+  
+result = mult(3, 5)
+print(result)
---
+++ 01-lessons/01/mystats.py
@@ -0,0 +1,5 @@
+def find_mean(li):
+    sum = 0
+    for num in li:
+        sum += num
+    return sum / len(li)
